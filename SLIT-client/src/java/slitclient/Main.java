5e8238547686c83b1a148c3be63d6dfbfcd976e8
piees <yngveranestad@gmail.com>
2015-11-05 13:44:55 +0100

    set gtk look and feel

--- SLIT-client/src/java/slitclient/Main.java
+++ SLIT-client/src/java/slitclient/Main.java
@@ -18,8 +18,7 @@ public class Main {
     
     public static void main(String[] args) {
         try {
-            UIManager.setLookAndFeel(
-            UIManager.getCrossPlatformLookAndFeelClassName());
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
