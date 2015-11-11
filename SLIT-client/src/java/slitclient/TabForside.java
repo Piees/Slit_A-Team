7c512dc4f1bcbc69a2b693225e5e3788f6b12210
piees <yngveranestad@gmail.com>
2015-11-11 11:37:57 +0100

    add copy to clipboard to contacts

--- SLIT-client/src/java/slitclient/TabForside.java
+++ SLIT-client/src/java/slitclient/TabForside.java
@@ -13,6 +13,9 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -407,6 +410,8 @@ public class TabForside {
 
         public void actionPerformed(ActionEvent e) {
             sendMail(mail);
+            Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
+            clpbrd.setContents(new StringSelection(mail), null);
         }
     }
 }
