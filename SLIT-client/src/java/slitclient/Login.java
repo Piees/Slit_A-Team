74b2c8b2ae4c8b0d0e1f324240b0cc818571e91f
ArildH <arildh93@gmail.com>
2015-11-16 18:03:57 +0100

    Remove code duplication by creating login-method which is called from both login-actions. Remove print statements

--- SLIT-client/src/java/slitclient/Login.java
+++ SLIT-client/src/java/slitclient/Login.java
@@ -1,8 +1,8 @@
 /**
  * This is the login screen that greets the user of the SLIT program.
- * 
- * The GUI design and logic is just blatantly ripped of a netbeans 
- * example, the only exception is the loginButtons ActionListener.
+ *
+ * The GUI design and logic is just blatantly ripped of a netbeans example, the
+ * only exception is the loginButtons ActionListener.
  *
  */
 package slitclient;
@@ -27,64 +27,33 @@ import prototypes.EditUser;
  *
  * @author Viktor Setervang
  */
-
 public class Login {
-    
+
     JTextField userText;
     JPasswordField passwordText;
     ActionListener loginAction;
     JFrame frame;
-    
-    public Login() {        
-       	frame = new JFrame("Login");
-	frame.setSize(300, 150);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	placeComponents(frame);
-	frame.setVisible(true);      
+
+    public Login() {
+        frame = new JFrame("Login");
+        frame.setSize(300, 150);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        placeComponents(frame);
+        frame.setVisible(true);
     }
-    
-    
+
     class LoginKeyAction implements KeyListener {
-        
+
         public LoginKeyAction() {
-            
+
         }
 
         @Override
         public void keyPressed(KeyEvent e) {
-            if(e.getKeyCode()==KeyEvent.VK_ENTER){
-            System.out.println("KEYPRESSED");
-            System.out.println("KEYPRESSED");
-            System.out.println("KEYPRESSED");
-            System.out.println("inside actionperformed");
-            String userName = userText.getText();
-            String pwd = passwordText.getText();
-            
-            EJBConnector ejbConnector = EJBConnector.getInstance();
-            dbConnectorRemote dbConnector = ejbConnector.getEjbRemote();
-            dbConnector.updateUsersHashMap();
-            HashMap<String, String> loginResult = dbConnector.login(userName, pwd);
-          
-            if (loginResult.size() > 1) {
-                if (loginResult.get("userType").equals("student")) {
-                    //create StudentGUI object with the list
-                    StudentGUI studentGUI = new StudentGUI(loginResult);
-                    frame.setVisible(false);
-                }
-                else if (loginResult.get("userType").equals("teacher") || 
-                        loginResult.get("userType").equals("helpTeacher")) {
-                        //create TeacherGUI object with the list
-                            TeacherGUI teacherGUI = new TeacherGUI(loginResult);
-                            frame.setVisible(false);
-                }
-                else {
-                    JOptionPane.showMessageDialog(null, "userType invalid, "
-                            + "contact the database manager");                    
-                }
-            }
-            else {
-                JOptionPane.showMessageDialog(null, loginResult.get("error1"));    
-            }
+            if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                String userName = userText.getText();
+                String pwd = passwordText.getText();
+                login(userName, pwd);
             }
         }
 
@@ -96,78 +65,71 @@ public class Login {
         public void keyTyped(KeyEvent e) {
         }
     }
-    
-    
-    class LoginAction implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            System.out.println("inside actionperformed");
-            String userName = userText.getText();
-            String pwd = passwordText.getText();
-            
-            EJBConnector ejbConnector = EJBConnector.getInstance();
-            dbConnectorRemote dbConnector = ejbConnector.getEjbRemote();
-            dbConnector.updateUsersHashMap();
-            HashMap<String, String> loginResult = dbConnector.login(userName, pwd);
-          
-            if (loginResult.size() > 1) {
-                if (loginResult.get("userType").equals("student")) {
-                    //create StudentGUI object with the list
-                    StudentGUI studentGUI = new StudentGUI(loginResult);
-                    frame.setVisible(false);
-                }
-                else if (loginResult.get("userType").equals("teacher") || 
-                        loginResult.get("userType").equals("helpTeacher")) {
-                        //create TeacherGUI object with the list
-                            TeacherGUI teacherGUI = new TeacherGUI(loginResult);
-                            frame.setVisible(false);
-                }
-                else {
-                    JOptionPane.showMessageDialog(null, "userType invalid, "
-                            + "contact the database manager");                    
-                }
+
+    public void login(String userName, String pwd) {
+        EJBConnector ejbConnector = EJBConnector.getInstance();
+        dbConnectorRemote dbConnector = ejbConnector.getEjbRemote();
+        dbConnector.updateUsersHashMap();
+        HashMap<String, String> loginResult = dbConnector.login(userName, pwd);
+
+        if (loginResult.size() > 1) {
+            if (loginResult.get("userType").equals("student")) {
+                //create StudentGUI object with the list
+                StudentGUI studentGUI = new StudentGUI(loginResult);
+                frame.setVisible(false);
+            } else if (loginResult.get("userType").equals("teacher")
+                    || loginResult.get("userType").equals("helpTeacher")) {
+                //create TeacherGUI object with the list
+                TeacherGUI teacherGUI = new TeacherGUI(loginResult);
+                frame.setVisible(false);
+            } else {
+                JOptionPane.showMessageDialog(null, "userType invalid, "
+                        + "contact the database manager");
             }
-            else {
-                JOptionPane.showMessageDialog(null, loginResult.get("error1"));    
-            }
+        } else {
+            JOptionPane.showMessageDialog(null, loginResult.get("error1"));
         }
-    };
-    
-    
+    }
 
     private void placeComponents(JFrame frame) {
-	frame.setLayout(null);
-	JLabel userLabel = new JLabel("User");
-	userLabel.setBounds(10, 10, 80, 25);
-	frame.add(userLabel);
-        
-	userText = new JTextField(20);
-	userText.setBounds(100, 10, 160, 25);
-	frame.add(userText);
+        frame.setLayout(null);
+        JLabel userLabel = new JLabel("User");
+        userLabel.setBounds(10, 10, 80, 25);
+        frame.add(userLabel);
 
-	JLabel passwordLabel = new JLabel("Password");
-	passwordLabel.setBounds(10, 40, 80, 25);
-	frame.add(passwordLabel);
+        userText = new JTextField(20);
+        userText.setBounds(100, 10, 160, 25);
+        frame.add(userText);
 
-	passwordText = new JPasswordField(20);
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 40, 80, 25);
+        frame.add(passwordLabel);
+
+        passwordText = new JPasswordField(20);
         passwordText.addKeyListener(new LoginKeyAction());
-	passwordText.setBounds(100, 40, 160, 25);
-	frame.add(passwordText);
+        passwordText.setBounds(100, 40, 160, 25);
+        frame.add(passwordText);
 
         JButton createButton = new JButton("create user");
-	createButton.setBounds(95, 80, 80, 25);
-	frame.add(createButton);
-        
-	JButton loginButton = new JButton("login");
-	loginButton.setBounds(10, 80, 80, 25);
-	frame.add(loginButton);
+        createButton.setBounds(95, 80, 80, 25);
+        frame.add(createButton);
 
-	JButton forgotPwdButton = new JButton("forgot my password!");
-	forgotPwdButton.setBounds(180, 80, 80, 25);
-	frame.add(forgotPwdButton);
-                
-        loginButton.addActionListener(new LoginAction()); //listener her
-                
+        JButton loginButton = new JButton("login");
+        loginButton.setBounds(10, 80, 80, 25);
+        frame.add(loginButton);
+
+        JButton forgotPwdButton = new JButton("forgot my password!");
+        forgotPwdButton.setBounds(180, 80, 80, 25);
+        frame.add(forgotPwdButton);
+
+//        loginButton.addActionListener(new LoginAction()); //listener her
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                login(userText.getText(), passwordText.getText());
+            }
+        });
+
         forgotPwdButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -178,14 +140,14 @@ public class Login {
 //                        + " button has been pressed");                
             }
         });
-        
-        createButton.addActionListener(new ActionListener(){
+
+        createButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ev) {
-                new CreateUser(); 
+                new CreateUser();
                 frame.dispose();
             }
-        });        
+        });
     }
 
 }
